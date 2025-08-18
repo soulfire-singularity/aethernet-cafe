72fb53cc1bf2b8770f66c333c3c8e5df2de83d24
Minerva <81447958+soulfire-singularity@users.noreply.github.com>
2025-08-17 22:03:09 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,19 @@
+// Generate random stars
+    const starField = document.getElementById("star-twinkle");
+const numStars = 100;
 
+for (let i = 0; i < numStars; i++) {
+  const star = document.createElement("div");
+  star.classList.add("star");
+
+  const size = Math.random() * 2 + 1 + "px";
+  star.style.width = size;
+  star.style.height = size;
+
+  star.style.top = Math.random() * 100 + "vh";
+  star.style.left = Math.random() * 100 + "vw";
+  star.style.animationDuration = (Math.random() * 3 + 2) + "s";
+  star.style.animationDelay = (Math.random() * 5) + "s";
+
+  starField.appendChild(star);
+};
